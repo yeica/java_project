c9adb9aabb651ce40866c0b4ffa01c6883dff99e
Yeyskalyn <yeyskalyn@gmail.com>
2021-06-25 19:18:21 -0400

    errores de bdd corregidos

--- src/screensframework/DBConnect/DBConnection.java
+++ src/screensframework/DBConnect/DBConnection.java
@@ -7,18 +7,13 @@ import java.sql.SQLException;
 public class DBConnection {
     
     private static Connection conn;
-    private static String url = "jdbc:postgresql://localhost:5432/ventas";
+    private static String url = "jdbc:postgresql://localhost:5432/JAVA_PROJECT_TEST";
     private static String user = "postgres";
-    private static String pass = "wilpolanco";
-    /*
-    private static String url = "jdbc:mysql://localhost/sysventas";
-    private static String user = "root";
-    private static String pass = "";*/
+    private static String pass = "yeika";
     
     public static Connection connect() throws SQLException{
 	try {
             Class.forName("org.postgresql.Driver").newInstance();
-            //Class.forName("com.mysql.jdbc.Driver").newInstance();
 	} catch(ClassNotFoundException cnfe) {
             System.err.println("Error: "+cnfe.getMessage());
 	} catch(InstantiationException ie) {
@@ -26,14 +21,16 @@ public class DBConnection {
 	} catch(IllegalAccessException iae) {
             System.err.println("Error: "+iae.getMessage());
 	}
-            conn = DriverManager.getConnection(url,user,pass);
-            return conn;
-	}
+        
+        conn = DriverManager.getConnection(url,user,pass);
+        return conn;
+    }
 	
     public static Connection getConnection() throws SQLException, ClassNotFoundException{
-        if(conn !=null && !conn.isClosed())
-            return conn;
+        if(conn == null && conn.isClosed()){
             connect();
-            return conn;
+        }
+        
+        return conn;
     }
 }
